46953bdda1610d1119dde32cd37cfe611be77d1c
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2025-11-05 01:41:03 -0300

    Add Messages controller with CRUD operations

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,50 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @messages = Message.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      redirect_to @message, notice: "Message was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to @message, notice: "Message was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to messages_url, notice: "Message was successfully deleted."
+  end
+
+  private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:subject, :body)
+  end
+end
